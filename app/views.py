6a689fb99d67255b142744fe4d51fe81f2fe2059
nziokaivy <nziokaivy@gmail.com>
2019-02-04 11:08:41 +0300

    import get sources function from request module

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from app import app
-
+from .request import get_sources
 # Views
 @app.route('/')
 def index():
@@ -8,6 +8,6 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-
+    general_category = get_sources('general')
     title = 'Home - The Best News Highlight Website Online'
-    return render_template('index.html', title = title)
+    return render_template('index.html', title = title, general = general_category)
