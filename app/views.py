a4fb05d6c1ff6e72424a0dc0c904e094bce49d2e
nziokaivy <nziokaivy@gmail.com>
2019-02-04 10:23:28 +0300

    add title to index.html page

--- app/views.py
+++ app/views.py
@@ -8,4 +8,6 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+
+    title = 'Home - The Best News Highlight Website Online'
+    return render_template('index.html', title = title)
