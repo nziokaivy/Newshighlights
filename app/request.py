3f1b1af47d5cc54a212dc3162469c585ceb48afd
nziokaivy <nziokaivy@gmail.com>
2019-02-04 10:54:48 +0300

    get source base url

--- app/request.py
+++ app/request.py
@@ -2,5 +2,12 @@ from app import app
 import urllib.request,json
 from .models import sources
 
+Sources = sources.Sources
+
 # Getting api key
 api_key = app.config['NEWS_API_KEY']
+
+# Getting the source base url
+base_url = app.config["SOURCES_BASE_URL"]
+
+
