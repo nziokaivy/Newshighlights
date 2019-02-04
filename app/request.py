413fe1ad7adf3458c51ced4de1a9be5e4b015feb
nziokaivy <nziokaivy@gmail.com>
2019-02-04 11:05:34 +0300

    add process results function

--- app/request.py
+++ app/request.py
@@ -11,3 +11,50 @@ api_key = app.config['NEWS_API_KEY']
 base_url = app.config["SOURCES_BASE_URL"]
 
 
+def get_sources(category):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_sources_url = base_url.format(category,api_key)
+
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
+
+        sources_results = None
+
+        if get_sources_response['sources']:
+            sources_results_list = get_sources_response['sources']
+            sources_results = process_results(get_sources_response['sources'])
+
+
+    return sources_results
+
+
+def process_results(sources_list):
+    '''
+    Function  that processes the source result and transform them to a list of Objects
+
+    Args:
+        sources_list: A list of dictionaries that contain sources details
+
+    Returns :
+        sources_results: A list of movie objects
+    '''
+    sources_results = []
+    for source in sources_list:
+        id = source.get('id')
+        name = source.get('name')
+        description = source.get('description')
+        url = source.get('url')
+        category = source.get('category')
+        country = source.get('country')
+
+        if url:
+            sources_object = Sources(id,name,description,url,category,country)
+            sources_results.append(sources_object)
+
+    return sources_results
+
+
+
